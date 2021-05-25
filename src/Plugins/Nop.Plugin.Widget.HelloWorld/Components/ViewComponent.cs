bc57bc8cfb67e2aa4b59a6dbd68d629269f6b234
Sakhawat Hosen <sakhawatcmt@outlook.com>
2021-05-25 19:53:26 +0600

    Initial customize in NopCommerce

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/Components/ViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widget.HelloWorld.Components
+{
+    [ViewComponent(Name = "HelloWorldWidget")]
+    public class ViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke(string widgetZone)
+        {
+            return Content("Hello Sakhawat. How are you today.");
+        }
+    }
+}
